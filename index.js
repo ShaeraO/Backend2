d10bfe95dd919c6d95e1e129e35157b5fef1c452
Shaera <amburcev2991@gmail.com>
2022-08-11 15:27:31 +0500

    commit

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.delete('/market/:id', checkAuth , CardController.remove)
 app.patch('/market/:id', checkAuth , handleValidErrors, CardController.update)
 app.get('/market/user/me', checkAuth, CardController.getMyCards)
 app.patch('/market/:id/like', checkAuth, CardController.like, )
-app.put('/market/:id/like/', checkAuth, CardController.likeDelete, )
+app.patch('/market/:id/likeremove', checkAuth, CardController.likeDelete, )
 app.get('/market/user/me/liked', checkAuth, CardController.getMyFavoriteCards)
 
 
