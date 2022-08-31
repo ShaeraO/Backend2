4410471ac75c2686fd4c16239dcba78f7f33a877
Shaera <amburcev2991@gmail.com>
2022-08-31 16:59:27 +0500

    commit

--- controllers/CardController.js
+++ controllers/CardController.js
@@ -121,8 +121,10 @@ export const create = async (req, res) => {
             author: req.userId,
             currency: req.body.currency,
             symbol: req.body.symbol,
+            delivery: [{
             destPrice: req.body.destPrice,
-            destination: req.body.destination,
+            destination: req.body.destination
+            }]
         })
 
         await UserSchema.findByIdAndUpdate(req.userId,{
