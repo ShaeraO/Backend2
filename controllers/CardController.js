d7fe76e44f83fe5923d50c5f6a1e86917e53a810
Shaera <amburcev2991@gmail.com>
2022-09-03 20:43:26 +0500

    comit

--- controllers/CardController.js
+++ controllers/CardController.js
@@ -284,7 +284,7 @@ export const getUserCards = async (req, res) => {
     try {
         const user = await UserSchema.findById(req.params.id)
         const list = await Promise.all(
-            user.cards.sort((a, b) => a.createdAt - b.createdAt).map((card) => {
+            user.cards.sort((a, b) => b.createdAt - a.createdAt).map((card) => {
                 return CardModel.findById(card._id).populate('author')
             }),
             )
