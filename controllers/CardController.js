907e308a381f5280fa6d14666a7a07b5c93dd037
Shaera <amburcev2991@gmail.com>
2022-09-03 17:50:53 +0500

    commit

--- controllers/CardController.js
+++ controllers/CardController.js
@@ -289,7 +289,7 @@ export const getUserCards = async (req, res) => {
         const list = await Promise.all(
             user.cards.map((card) => {
                 return CardModel.findById(card._id).populate('author').sort({
-                    createdAt: -1
+                    createdAt: 1
                 })
             }),
             )
